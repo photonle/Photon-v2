98ecbf047f3b23da9f7fa76f8f83255635f51d50
DatedGore <isaiahpohlman@gmail.com>
2024-02-11 13:33:11 -0500

    SOS MPF3
    SOS MPF3, added code + made shape/detail

--- lua/photon-v2/library/components/photon_sos_mpf3.lua
+++ lua/photon-v2/library/components/photon_sos_mpf3.lua
@@ -13,10 +13,81 @@ COMPONENT.PrintName = [[SoundOff Signal mpower Fascia 3"]]
 
 COMPONENT.Model = "models/sentry/props/soundofffascia_small.mdl"
 
-COMPONENT.Templates = {}
+COMPONENT.States = {
+	[1] = "R",
+	[2] = "B",
+	[3] = "W"
+}
 
-COMPONENT.Elements = {}
+local s = 1.6
 
-COMPONENT.Segments = {}
+COMPONENT.Templates = {
+	["2D"] = {
+		Light = {
+			Shape = PhotonMaterial.GenerateLightQuad("photon/lights/sos_mpf3_shape.png").MaterialName,
+			Detail = PhotonMaterial.GenerateLightQuad("photon/lights/sos_mpf3_detail.png").MaterialName,
+			Width = 6,
+			Height = 6,
+			Ratio = 1,
+			Scale = 1.4,
+			ForwardVisibilityOffset = -0.1,
+			ForwardBloomOffset = 0.1,
+		}
+	}
+}
 
-COMPONENT.Inputs = {}
+COMPONENT.StateMap = "[1/2/3] 1"
+
+COMPONENT.Elements = {
+	[1] = { "Light", Vector(0.5, 0, 0), Angle(0, -90, 0) },
+}
+
+local sequence = Photon2.SequenceBuilder.New
+
+COMPONENT.Segments = {
+	Light = {
+		Frames = {
+			[1] = "[1] 1",
+			[2] = "[2] 1",
+			[3] = "[3] 1",
+			[4] = "[W] 1",
+			[5] = "[R] 1"
+			-- [1] = { { 1, 4 } },
+		},
+		Sequences = {
+			["TEST"] = { 
+				1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+				2, 2, 2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+				3, 3, 0, 3, 3, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+			},
+			["MODE3"] = sequence():Alternate( 1, 2, 8 ),
+			["MODE3:A"] = { 1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0 },
+			["MODE3:B"] = { 0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0 },
+			["W"] = { 4 },
+			["R"] = { 5 },
+			["1"] = { 1 },
+			["2"] = { 2 },
+			["3"] = { 3 },
+
+		}
+	}
+}
+
+COMPONENT.Inputs = {
+	["Emergency.Warning"] = {
+		["MODE1"] = {
+			Light = "MODE3"
+		},
+		["MODE2"] = {
+			Light = "MODE3"
+		},
+		["MODE3"] = {
+			Light = "MODE3"
+		}
+	},
+	["Emergency.Marker"] = {
+		["ON"] = {
+			Light = "1"
+		}
+	}
+}
