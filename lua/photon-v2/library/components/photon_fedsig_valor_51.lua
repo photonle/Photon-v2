6d9dda6ed02f425e1167c766fbc28cd80650b3ed
Alex Smith <ischmal001@gmail.com>
2024-03-18 07:49:23 -0600

    Add new Valor 51" model
    - Materials are still a work in progress

---
+++ lua/photon-v2/library/components/photon_fedsig_valor_51.lua
@@ -0,0 +1,47 @@
+if (Photon2.ReloadComponentFile()) then return end
+local COMPONENT = Photon2.LibraryComponent()
+local sequence = Photon2.SequenceBuilder.New
+
+COMPONENT.Author = "Photon"
+
+COMPONENT.Credits = {
+	Model = "Twurtleee, Schmal",
+	Code = "Schmal"
+}
+
+COMPONENT.Title = [[Federal Signal Valor (51")]]
+COMPONENT.Category = "Lightbar"
+COMPONENT.Model = "models/schmal/fedsig_valor_51.mdl"
+
+COMPONENT.Preview = {
+	Position = Vector( 0, 0, -0.5 ),
+	Angles = Angle( 0, 180, 0 ),
+	Zoom = 1.2
+}
+
+COMPONENT.Templates = {
+	["2D"] = {
+
+	}
+}
+
+COMPONENT.Elements = {
+
+}
+
+COMPONENT.StateMap = ""
+
+COMPONENT.Segments = {
+	All = {
+		Frames = {
+			[1] = "",
+		},
+		Sequences = {
+			["ON"] = { 1 },
+		}
+	},
+}
+
+COMPONENT.Inputs = {
+
+}
