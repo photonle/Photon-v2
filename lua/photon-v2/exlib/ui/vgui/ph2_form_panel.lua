9b404e27bdce6c8c6639b4dec06b4ae59fdc8f04
Alex Smith <ischmal001@gmail.com>
2024-05-23 21:28:48 -0600

    Improve Photon form panel

--- lua/photon-v2/exlib/ui/vgui/ph2_form_panel.lua
+++ lua/photon-v2/exlib/ui/vgui/ph2_form_panel.lua
@@ -1,6 +1,8 @@
 local class = "Photon2UIFormPanel"
-local base = "DPanel"
-local description = "Basic key-value form."
+local base = "DScrollPanel"
+local description = "Generic form because DForm is an unmaintained shit show."
+
+local callbackIndex = 1
 
 ---@class Photon2UIFormPanel : Panel
 local PANEL = {}
@@ -13,8 +15,24 @@ PANEL.ItemHeight = 32
 
 function PANEL:Init()
 	self.Items = {}
+	self.Properties = {}
+	self:SetPaintBackground( false )
 end
 
+function PANEL:AddDivider()
+	local panel = vgui.Create( "DPanel", self )
+	panel:Dock( TOP )
+	panel:SetHeight( 1 )
+	panel:SetPaintBackground( false )
+	panel:DockMargin( 0, 4, 0, 4 )
+	function panel:Paint( w, h )
+		surface.SetDrawColor( 0, 0, 0, 128 )
+		surface.DrawRect( 0, 0, w, h )
+	end
+	return panel
+end
+
+-- Internal function called by each new form property added.
 function PANEL:CreateBaseProperty( propertyName, labelText )
 
 	if ( IsValid( self.Items[propertyName] ) ) then
@@ -25,15 +43,15 @@ function PANEL:CreateBaseProperty( propertyName, labelText )
 	panel:SetHeight( self.ItemHeight )
 	panel:Dock( TOP )
 	panel:SetPaintBackground( false )
-	panel:DockPadding( 0, 4, 0, 4 )
+	panel:DockPadding( 4, 4, 4, 4 )
 	self.Items[propertyName] = panel
 
 	local label = vgui.Create( "DLabel", panel )
 	label:SetWidth( self.LabelWidth )
-	label:SetContentAlignment( 6 )
+	label:SetContentAlignment( 4 )
 	label:SetText( labelText )
 	label:Dock( LEFT )
-	label:SetTextInset( 8, 0 )
+	label:SetTextInset( 4, 0 )
 
 	panel.Label = label
 
@@ -46,6 +64,7 @@ function PANEL:CreateBaseProperty( propertyName, labelText )
 	return panel
 end
 
+-- Simple text-box property.
 function PANEL:CreateTextEntryProperty( propertyName, labelText, currentValue, params )
 	local this = self
 	local panel = self:CreateBaseProperty( propertyName, labelText )
@@ -64,7 +83,7 @@ function PANEL:CreateTextEntryProperty( propertyName, labelText, currentValue, p
 	panel:SetValue( currentValue )
 
 	function textEntry:OnValueChange( value )
-		this:OnPropertyChanged( propertyName, value )
+		this:InternalOnPropertyChanged( propertyName, value )
 	end
 end
 
@@ -79,6 +98,7 @@ end
 
 function PANEL:CreateLibraryEntryProperty( propertyName, labelText, currentValue, params )
 	local panel = self:CreateBaseProperty( propertyName, labelText )
+	local this = self
 	local textEntry = vgui.Create( "DTextEntry", panel.Content )
 	textEntry:Dock( FILL )
 	textEntry:SetUpdateOnType( true )
@@ -93,7 +113,7 @@ function PANEL:CreateLibraryEntryProperty( propertyName, labelText, currentValue
 	panel:SetValue( currentValue )
 
 	function textEntry:OnValueChange( value )
-		this:OnPropertyChanged( propertyName, value )
+		this:InternalOnPropertyChanged( propertyName, value )
 	end
 
 	local browse = vgui.Create( "DButton", panel.Content )
@@ -114,9 +134,201 @@ function PANEL:CreateLibraryEntryProperty( propertyName, labelText, currentValue
 
 end
 
+
+---@param propertyName string
+---@param labelText string
+---@param currentValue boolean
+---@param params? { Descriptor?: string }
+function PANEL:CreateCheckBoxProperty( propertyName, labelText, currentValue, params )
+	local panel = self:CreateBaseProperty( propertyName, labelText )
+	local descriptor = ""
+	if ( params ) then
+		if ( params.Descriptor ) then
+			descriptor = params.Descriptor
+		end
+	end
+	local this = self
+	
+	local checkbox = vgui.Create( "DCheckBoxLabel", panel.Content )
+	checkbox:Dock( FILL )
+	checkbox:SetText( tostring( descriptor ) )
+	checkbox:SetChecked( currentValue or false )
+
+	function panel:SetValue( value )
+		print( "Setting value to " .. tostring( value ) )
+		if ( isstring( value ) ) then value = ( value == "1" ) end
+		if ( value == self:GetValue() ) then return end
+		checkbox:SetChecked( value )
+	end
+
+	function panel:GetValue()
+		return checkbox:GetChecked()
+	end
+
+	function checkbox:OnChange( value )
+		this:InternalOnPropertyChanged( propertyName, value )
+	end
+end
+
+function PANEL:CreateComboBoxProperty( propertyName, labelText, currentValue, options )
+	local panel = self:CreateBaseProperty( propertyName, labelText )
+	local this = self
+	local comboBox = vgui.Create( "DComboBox", panel.Content )
+	comboBox:Dock( FILL )
+	comboBox:SetValue( currentValue or "" )
+
+	local mapIn, mapOut
+
+	for k, v in pairs( options or {} ) do
+		if ( isstring( v ) ) then
+			comboBox:AddChoice( v )
+		elseif ( istable( v ) ) then
+			mapIn = mapIn or {}
+			mapOut = mapOut or {}
+			mapOut[v[1]] = v[2]
+			mapIn[v[2]] = v[1]
+			comboBox:AddChoice( v[1], v[2] )
+		end
+	end
+
+	function panel:SetValue( value )
+		if ( mapIn ) then
+			value = mapIn[value] or value
+		end
+		comboBox:SetValue( value )
+	end
+
+	function panel:GetValue()
+		local result = comboBox:GetValue()
+		if ( mapOut ) then
+			result = mapOut[result] or result
+		end
+		return result
+	end
+
+	function comboBox:OnSelect( index, value, data )
+		if ( mapOut ) then
+			value = mapOut[value] or value
+		end
+		this:InternalOnPropertyChanged( propertyName, value )
+	end
+end
+
+function PANEL:CreateNumberSliderProperty( propertyName, labelText, currentValue, min, max, decimals )
+	local panel = self:CreateBaseProperty( propertyName, labelText )
+	local this = self
+	local slider = vgui.Create( "DNumberWang", panel.Content )
+	slider:Dock( FILL )
+	slider:SetMinMax( min, max )
+	slider:SetDecimals( decimals or 0 )
+	slider:SetValue( currentValue or 0 )
+
+	function panel:SetValue( value )
+		slider:SetValue( value )
+	end
+
+	function panel:GetValue()
+		return slider:GetValue()
+	end
+
+	function slider:OnValueChanged( value )
+		this:InternalOnPropertyChanged( propertyName, value )
+	end
+end
+
+function PANEL:AddButton( text, callback, icon )
+	local panel = vgui.Create( "DPanel", self )
+	panel:Dock( TOP )
+	panel:SetHeight( self.ItemHeight )
+	panel:SetPaintBackground( false )
+	panel:DockPadding( 4, 4, 4, 4 )
+
+	local button = vgui.Create( "EXDButton", panel )
+	button:Dock( RIGHT )
+	button:SetText( text )
+	button:SetHeight( self.ItemHeight )
+	button.DoClick = callback
+	button:SizeToContentsX( 32 )
+	if ( icon ) then
+		button:SetIcon( icon )
+	end
+
+end
+
+---comment
+---@param name string Internal property name.
+---@param onChange fun(value: any) Function to call when the property changes.
+---@param convertInput? fun(value: any): any Function to convert the external value to the one internally displayed.
+---@param convertOutput? fun(value: any): any Function to convert the internally displayed value to the external one.
+function PANEL:RegisterProperty( name, onChange, convertInput, convertOutput )
+	self.Properties[name] = {
+		OnChange = onChange or function() end,
+		Input = convertInput or function( val ) return val end,
+		Output = convertOutput or function( val ) return val end
+	}
+end
+
+
+---comment
+---@param name any
+---@param type? "String" | "Int" | "Float" | "Bool"
+---@param onChange? any
+---@param convertInput? any
+---@param convertOutput? any
+function PANEL:RegisterCVarProperty( name, type, onChange, convertInput, convertOutput )
+	callbackIndex = callbackIndex + 1
+	local thisCallbackIndex = callbackIndex
+	self.Properties[name] = {
+		IsCVar = thisCallbackIndex,
+		CVar = GetConVar( name ),
+		Type = type or "String",
+		OnChange = onChange or function() end,
+		Input = convertInput or function( val ) return val end,
+		Output = convertOutput or function( val ) return val end
+	}
+	self.Properties[name].GetCVar = self.Properties[name].CVar["Get" .. self.Properties[name].Type]
+	self.Properties[name].SetCVar = self.Properties[name].CVar["Set" .. self.Properties[name].Type]
+	cvars.AddChangeCallback( name, function( convar, oldValue, newValue )
+		if ( not IsValid( self ) ) then
+			-- this has to be delayed or else it removes the entry from the callback table
+			-- while it's still being iterated through (which really fucks things up)
+			timer.Simple( 0.1, function() 
+				cvars.RemoveChangeCallback( convar, tostring( thisCallbackIndex ) )
+			end)
+			return
+		end
+		print( "CVar changed: " .. tostring( convar ) .. " " .. tostring( oldValue ) .. " " .. tostring( newValue ) )
+		if ( self.Items[name] ) then
+			self.Items[name]:SetValue( self.Properties[name].Input( newValue ) )
+		end
+	end, tostring( thisCallbackIndex ) )
+	timer.Simple( 0, function()
+		if ( not IsValid( self ) ) then return end
+		if ( self.Items[name] ) then
+			self.Items[name]:SetValue( self.Properties[name].Input( 
+				self.Properties[name].GetCVar( self.Properties[name].CVar )
+			 ) )
+		end
+	end)
+end
+
+function PANEL:InternalOnPropertyChanged( name, value )
+	local property = self.Properties[name]
+	if ( property ) then
+		if ( property.Input ) then
+			value = property.Input( value )
+		end
+		property.OnChange( value )
+		if ( property.CVar ) then
+			property.CVar["Set" .. property.Type]( property.CVar, value )
+		end
+	end
+	self:OnPropertyChanged( name, value )
+end
+
 --- For overriding
 function PANEL:OnPropertyChanged( name, value )
-	print("On property changed")
+	print( string.format( "Property [%s] changed to [%s]", name, value ) )
 end
 
 derma.DefineControl( class, description, PANEL, base )
