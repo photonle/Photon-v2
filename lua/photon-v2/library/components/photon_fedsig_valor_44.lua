54123d6dcc4180b4c894bde4d85227d56a8357dc
Alex Smith <ischmal001@gmail.com>
2024-06-12 04:48:05 -0600

    Add initial patterns/functional for Valor 44"

--- lua/photon-v2/library/components/photon_fedsig_valor_44.lua
+++ lua/photon-v2/library/components/photon_fedsig_valor_44.lua
@@ -50,7 +50,7 @@ COMPONENT.DefineOptions = {
 			self.BodyGroups = self.BodyGroups or {}
 			self.BodyGroups["alley_hotfeet"] = enabled and 0 or 1
 		end
-	}
+	},
 }
 
 COMPONENT.Templates = {
@@ -80,6 +80,17 @@ COMPONENT.Templates = {
 				-- [2] = 0
 			-- }
 		}
+	},
+	["Projected"] = {
+		TakedownIllumination = {
+			Material = "photon/flashlight/led_linear.png",
+			Brightness = 1.5,
+			FOV = 45
+		},
+		FloodIllumination = {
+			Material = "photon/flashlight/led_linear.png",
+			Brightness = 4
+		},
 	}
 }
 
@@ -125,28 +136,201 @@ COMPONENT.Elements = {
 
 	[27] = { "HotFoot", Vector( 1.62, -0.1, -.35 ), Angle( 0, -95.4, 0 ), BoneParent = "valor_44_hootfoot_alley_left", Width = 4.7, RequiredBodyGroups = { ["alley_hotfeet"] = 0 } },
 	[28] = { "HotFoot", Vector( 1.62, -0.1, -.35 ), Angle( 0, -95.4, 0 ), BoneParent = "valor_44_hootfoot_alley_right", Width = 4.7, RequiredBodyGroups = { ["alley_hotfeet"] = 0 } },
+
+	[29] = { "TakedownIllumination",  Vector( -0.64, 19.5, 1.2 ), Angle( 0, -90, 0 ) },
+	[30] = { "TakedownIllumination",  Vector( -0.64, -19.5, 1.2 ), Angle( 0, -90, 0 ) },
+
+	[31] = { "TakedownIllumination", Vector( -7.06, 26.7, 1.2 ), Angle( 0, -11, 0 ) },
+	[32] = { "TakedownIllumination", Vector( -7.06, -26.7, 1.2 ), Angle( 0, 180 + 11, 0 ) },
+
+	[33] = { "FloodIllumination", Vector( 10, 0, 1.2 ), Angle( 0, -90, 0 ), FOV = 90, Brightness = 1.5, Material = "photon/flashlight/wide.png" }
+	
 }
 
-COMPONENT.StateMap = "[R] 1 3 5 7 9 11 13 15 17 19 21 23 25 27 [B] 2 4 6 8 10 12 14 16 18 20 22 24 26 28"
+COMPONENT.StateMap = "[R] 1 3 5 7 9 11 13 15 17 19 21 23 25 27 [B] 2 4 6 8 10 12 14 16 18 20 22 24 26 28 [W] 29 30 31 32 33"
+
+local sequence = Photon2.SequenceBuilder.New
 
 COMPONENT.Segments = {
 	All = {
 		Frames = {
 			[1] = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28",
 			[2] = "1 3 5 7 9 11 13 15 17 19 21 23 25 27",
-			[3] = "2 4 6 8 10 12 14 16 18 20 22 24 26 28"
+			[3] = "2 4 6 8 10 12 14 16 18 20 22 24 26 28",
+			[4] = "1 5 9 13 17 21 24 20 16 12 8 4",
+			[5] = "2 6 10 14 18 22 23 19 15 11 7 3"
+			
 		},
 		Sequences = {
 			["ON"] = { 1 },
-			["ALT"] = sequence():Alternate( 2, 3 ):SetTiming( 1 )
+			["TRI_FLASH"] = sequence():TripleFlash( 2, 3 ),
+			["QUAD_FLASH"] = sequence():QuadFlash( 2, 3 ),
+			["ALT"] = sequence():Alternate( 2, 3 ):SetTiming( 1 ),
+			["MIX"] = sequence():FlashHold( { 4, 5 }, 2, 3 )
 		}
 	},
+	Rear = {
+		Frames = {
+			[1] = "15 17 19 24 22 20",
+			[2] = "20 18 16 21 23 19",
+		},
+		Sequences = {
+			["ON"] = { 1 },
+			["FLASH"] = sequence():FlashHold( { 1, 2 }, 3, 3 )
+		}
+	},
+	["Pursuit"] = {
+		Frames = {
+			[0] = "[PASS] 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24",
+			[1] = "1 5 9 13 17 21 25 4 8 12 16 20 24",
+			[2] = "3 7 11 15 19 23 2 6 10 14 18 22",
+			[3] = "1 3 5 7 9 11 13 15 17 19 21 23",
+			[4] = "2 4 6 8 10 12 14 16 18 20 22 24"
+		},
+		Sequences = {
+			["PURSUIT"] = sequence()
+							:Alternate( 3, 4, 6 )
+							:Alternate( 3, 4, 3 ):Do( 5 )
+							:Alternate( 1, 2, 3 ):Do( 3 )
+							:Alternate( 3, 4, 3 ):Do( 5 )
+							:FlashHold( 1, 3, 2 )
+							:FlashHold( 2, 3, 2 )
+						}
+	},
+	["White"] = {
+		Frames = {
+			[1] = "[W] 1 5 9 13 4 8 12",
+			[2] = "[W] 3 7 11 2 6 10 14",
+			[3] = "[W] 1 3 5 7 9 11 13",
+			[4] = "[W] 2 4 6 8 10 12 14"
+		},
+		Sequences = {
+			["PURSUIT"] = sequence()
+							:Alternate( 4, 3, 6 ):Add( 0 )
+							:Alternate( 4, 3, 3 ):Add( 0 ):Do( 5 )
+							:Alternate( 2, 1, 3 ):Add( 0 ):Do( 3 )
+							:Alternate( 4, 3, 3 ):Add( 0 ):Do( 5 )
+							:FlashHold( 2, 3, 1 ):Add( 0 )
+							:FlashHold( 1, 3, 1 ):Add( 0 ),
+			["OFF"] = {}
+		}
+	},
+	["Takedown"] = {
+		Frames = {
+			[1] = "[W] 10 9 25 26 29 30"
+		},
+		Sequences = {
+			["TAKEDOWN"] = { 1 }
+		}
+	},
+	["Flood"] = {
+		Frames = {
+			[1] = "[W] 1 2 3 4 5 6 7 8 9 10 11 12 25 26 33"
+		},
+		Sequences = {
+			["FLOOD"] = { 1 }
+		}
+	},
+	["LeftAlley"] = {
+		Frames = {
+			[1] = "[W] 11 13 31"
+		},
+		Sequences = {
+			["ON"] = { 1 }
+		}
+	},
+	["RightAlley"] = {
+		Frames = {
+			[1] = "[W] 12 14 32"
+		},
+		Sequences = {
+			["ON"] = { 1 }
+		}
+	},
+	["Marker"] = {
+		Frames = {
+			[1] = "9 10 11 12 13 14 15 16",
+		},
+		Sequences = {
+			["ON"] = { 1 },
+		}
+	},
+	["FrontCut"] = {
+		Frames = {
+			[1] = "[OFF] 1 2 3 4 5 6 7 8 9 10 11 12 13 14 25 26 27 28 29 30 31 32 33",
+		},
+		Sequences = {
+			["ENABLED"] = { 1 },
+		}
+	},
+	["RearCut"] = {
+		Frames = {
+			[1] = "[OFF] 15 16 17 18 19 20 21 22 23 24",
+		},
+		Sequences = {
+			["ENABLED"] = { 1 },
+		}
+	},
+	["SignalMaster"] = Photon2.SegmentBuilder.SignalMaster( 17, 19, 21, 23, 24, 22, 20, 18 )
+}
+
+COMPONENT.Patterns = {
+	["PURSUIT"] = {
+		{ "Pursuit", "PURSUIT" },
+	},
+	["PURSUIT_W"] = {
+		{ "White", "PURSUIT" },
+		{ "Pursuit", "PURSUIT" },
+	},
+}
+
+COMPONENT.Features = {
+	ParkMode = true
 }
 
 COMPONENT.Inputs = {
 	["Emergency.Warning"] = {
-		["MODE1"] = { All = "ON" },
-		["MODE2"] = { All = "ON" },
-		["MODE3"] = { All = "ON" },
-	}
+		["MODE1"] = { 
+			Rear = "FLASH" 
+		},
+		["MODE2"] = { 
+			All = "QUAD_FLASH" 
+		},
+		["MODE3"] = "PURSUIT_W"
+	},
+	["Emergency.ParkedWarning"] = {
+		["MODE3"] = {
+			White = "OFF"
+		}
+	},
+	["Emergency.SceneForward"] = {
+		["ON"] = {
+			["Takedown"] = "TAKEDOWN",
+		},
+		["FLOOD"] = {
+			["Flood"] = "FLOOD"
+		}
+	},
+	["Emergency.SceneLeft"] = {
+		["ON"] = { LeftAlley = "ON" }
+	},
+	["Emergency.SceneRight"] = {
+		["ON"] = { RightAlley = "ON" }
+	},
+	["Emergency.Directional"] = {
+		["LEFT"] = { SignalMaster = "LEFT" },
+		["RIGHT"] = { SignalMaster = "RIGHT" },
+		["CENOUT"] = { SignalMaster = "CENOUT" },
+	},
+	["Emergency.Marker"] = {
+		["ON"] = { Marker = "ON" }
+	},
+	["Emergency.Cut"] = {
+		["FRONT"] = { FrontCut = "ENABLED" },
+		["REAR"] = { RearCut = "ENABLED" }
+	},
+}
+
+COMPONENT.Options = {
+	-- Test = "ON"
 }
